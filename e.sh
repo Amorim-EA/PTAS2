0822c92914884c7de3c51994ca990c257b4c9fac
Luiz F. Picolo <luizpicolo@gmail.com>
2023-05-22 14:30:44 -0400

    Tudo rodando 100%.

--- e.sh
+++ e.sh
@@ -2,26 +2,14 @@
 
 git init 
 
-echo "Escreva seu nome de usuário"
-read u_name
-git config user.name $u_name
+git config user.name @username
 
+git config user.email @email
 
-echo "Escreva seu nome de email"
-read u_email
-git config user.email $u_email
-
-echo "Cole o link do Repositório do git"
-read ul_repo
-git remote add origin $ul_repo
+git remote add origin @repo_name
 
 git add .
-git commit -m "Commit inicial" 
-
-echo "Entre nesse site:"
-echo "https://github.com/$u_name/settings/tokens"
-echo"Crie um token e salve-o" 
-echo "Copie e depois cole/PASTE o seu token no campo que solicita o password" 
+git commit -m "@message" 
 
 git push origin master 
 
